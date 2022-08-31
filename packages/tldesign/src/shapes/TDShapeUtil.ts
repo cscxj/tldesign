18da157f17523b944f1fbb9dc7908c308435388f
cxj <2191505874@qq.com>
2022-08-31 23:26:58 +0800

    feat: 删除原来的intersect代码

--- packages/tldesign/src/shapes/TDShapeUtil.ts
+++ packages/tldesign/src/shapes/TDShapeUtil.ts
@@ -1,6 +1,7 @@
 import { TDShape, TDShapeType } from '@/types'
-import { Intersect, Utils } from '@/utils'
-import { TLBounds, TLShapeUtil } from '@tldesign/core'
+import { Utils } from '@/utils'
+import Intersect from '@tldesign/intersect'
+import { Point, TLBounds, TLShapeUtil } from '@tldesign/core'
 
 export abstract class TDShapeUtil<S extends TDShape> extends TLShapeUtil<S> {
   abstract type: TDShapeType
@@ -14,8 +15,25 @@ export abstract class TDShapeUtil<S extends TDShape> extends TLShapeUtil<S> {
     return corners.every((point) => {
       return (
         Utils.pointInBounds(point, bounds) ||
-        Intersect.intersectPolygonBounds(corners, bounds).length > 0
+        intersectPolygonBounds(corners, bounds).length > 0
       )
     })
   }
+}
+
+/**
+ * 查找多边形和bounds的交点
+ * @param points
+ * @param bounds
+ * @return 所有交点
+ */
+export function intersectPolygonBounds(
+  points: Point[],
+  bounds: TLBounds
+): Point[] {
+  return Intersect.intersectRectanglePolygon(
+    [bounds.x, bounds.y],
+    [bounds.width, bounds.height],
+    points
+  )
 }
--- packages/tldesign/src/utils/index.ts
+++ packages/tldesign/src/utils/index.ts
@@ -1,3 +1,2 @@
 export * as Snapshot from './snapshot'
 export * as Utils from './utils'
-export * as Intersect from './intersect'
